c9d4364a18a0bcb5079013a7753fe70b3fe39c07
Charifa Znifech <charifaznifech01@gmail.com>
2025-03-11 09:58:00 +0100

    Add project files.

---
+++ Epreuve-WAD24-ASP.NET-MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Epreuve_WAD24_ASP.NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epreuve_WAD24_ASP.NET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
